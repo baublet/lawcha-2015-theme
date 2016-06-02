e3a699e345576170d40dacaf453f2c2ff4e8aa42
Ryan Poe <baublet@gmail.com>
2016-06-02 11:16:53 -0400

    Re-up Masonry for authors boxes

--- page-templates/page_laboronline.php
+++ page-templates/page_laboronline.php
@@ -3,6 +3,7 @@
 
  // Turns off AutoP for these pages
 remove_filter('the_content', 'wpautop');
+wp_enqueue_script('masonry', 'https://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.1/masonry.pkgd.min.js', array(), '1.0.0', true );
 
 get_header(); ?>
 
@@ -22,5 +23,19 @@ get_header(); ?>
 
     </main><!-- .site-main -->
   </div><!-- .content-area -->
+
+  <script>
+    window.onload = function() {
+        var elem = document.querySelector('#authors-grid');
+        var msnry = new Masonry( elem, {
+            // options
+            itemSelector: '.an-author-box',
+            columnWidth: 64,
+            gutter: 12,
+            isFitWidth: true,
+            transitionDuration: '0.2s'
+        });
+    };
+  </script>
 
 <?php get_footer(); ?>
